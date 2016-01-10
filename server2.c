95ff336e8a999f1afede744ef0ec1a6f3cef0e6b
fanyui <fanyui@localhost.localdomain>
2016-01-09 23:17:02 -0500

    server and clent program in socket programming

---
+++ server2.c
@@ -0,0 +1,38 @@
+//SERVER PROGRAM USING AF_INET AND SOCK_STREAM TO COMMUNICATE WITH DEFAULT IP 127.0.0.1
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+int main()
+{
+int server_sockfd, client_sockfd;
+int server_len, client_len;
+struct sockaddr_in server_address;
+struct sockaddr_in client_address;
+
+server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+server_address.sin_family = AF_INET;
+server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+server_address.sin_port = htons(8080);
+server_len = sizeof(server_address);
+
+bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+
+listen(server_sockfd, 5);
+while(1) {
+char ch;
+printf("server waiting\n");
+client_len = sizeof(client_address);
+client_sockfd = accept(server_sockfd,
+(struct sockaddr *)&client_address, &client_len);
+read(client_sockfd, &ch, 1);
+printf("%c",ch);
+ch++;
+sleep(5);
+write(client_sockfd, &ch, 1);
+close(client_sockfd);
+}
+}
